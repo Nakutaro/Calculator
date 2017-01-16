7aa88ebb5721536bfebfc2ce4143cfed3f2877ea
https://github.com/Nakutaro/Calculator.git <nakutaro@bk.ru>
2017-01-16 23:49:07 +0400

    working version

---
+++ Calculator/Main.cs
@@ -0,0 +1,32 @@
+﻿using CalculatorHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator(new CalculatorHelper.IOperation[] {
+                new CalculatorHelper.SumOperation(),
+                new CalculatorHelper.MultiplicationOperation(),
+                new CalculatorHelper.CubeOperation(),
+                new CalculatorHelper.ExpOperation()
+                });
+
+            var Sum = calc.Execute("Sum", new object[] { 1, 2 });
+            var Multiplication = calc.Execute("Multiplication", new object[] { 3, 4 });
+            var Cube = calc.Execute("Cube", new object[] { 6 });
+            var Exp = calc.Execute("Exp", new object[] { null });
+            Console.WriteLine($"result Sum = {Sum}");
+            Console.WriteLine($"result Multiplication = {Multiplication}");
+            Console.WriteLine($"result Cube = {Cube}");
+            Console.WriteLine($"result Exp = {Exp}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassLibrary1/CalculatorHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorHelper
+{
+    public class Calculator
+    {
+        public Calculator(IOperation[] opers)
+        {
+            operations = opers;
+        }
+        private IOperation[] operations { get; set; }
+
+        public object Execute(string name, object[] args)
+        {
+            var oper = operations.FirstOrDefault(o => o.Name == name);
+            return oper.Execute(args);
+        }
+    }
+
+    public interface IOperation //только описание мтодов в intarface
+    {
+        string Name { get; }
+        object Execute(object[] args);
+    }
+
+    public class SumOperation : IOperation//операция суммирования аргументов
+    {
+        public string Name { get { return "Sum"; } }
+        public object Execute(object[] args)
+        {
+            return (int)args[0] + (int)args[1];
+        }
+    }
+
+    public class MultiplicationOperation : IOperation//операция перемножения аргументов
+    {
+        public string Name { get { return "Multiplication"; } }
+        public object Execute(object[] args)
+        {
+            return (int)args[0] * (int)args[1];
+        }
+    }
+
+    public class CubeOperation : IOperation//аргумент в кубе
+    {
+        public string Name { get { return "Cube"; } }
+        public object Execute(object[] args)
+        {
+            return (int)args[0] * (int)args[0]* (int)args[0];
+        }
+    }
+
+    public class ExpOperation : IOperation//экспонента в n-ой степени, преобразованная в целое число
+    {
+        public string Name { get { return "Exp"; } }
+        public object Execute(object[] args)
+        {
+            int n = 10;
+            return (int)(Math.Exp(n));
+        }
+    }
+}
